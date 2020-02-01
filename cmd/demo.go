967c0b2fe306d46fe277f45ba56609753e0fff65
MecaCho <1099833328@qq.com>
2020-02-02 01:39:04 +0800

    add config;

--- cmd/demo.go
+++ cmd/demo.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	_ "github.com/jinzhu/gorm"
 	"web_server_gin/config"
@@ -12,7 +13,10 @@ func main() {
 	engine := gin.Default()
 	engine.Any("/", controller.HelloController)
 	conf := config.InitConfig()
-	v1.RouterGroup(conf.DBConn, engine)
 
-	engine.Run(":80")
+	if err := v1.RouterGroup(conf.DBConn, engine); err != nil {
+		panic(err)
+	}
+
+	engine.Run(fmt.Sprintf("%s:%d", conf.ServerIP, conf.ServerPort))
 }
---
+++ scripts/start_server.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+export DBCON="root:xxxxxx@tcp(127.0.0.1:3306)/blog?charset=utf8&loc=Asia%2FShanghai"
+export IP=0.0.0.0
+export PORT=80
+export LOG_FILE=demo.log
+
+/usr/bin/demo --alsologtostderr=true \
+--serverIp=${IP} \
+--serverPort=${PORT} \
+--dbConnect=${DBCON} \
+--debug=false \
+>> ${LOG_FILE} 2>&1 &
