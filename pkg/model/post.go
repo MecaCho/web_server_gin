8b1a0d964da4b5fcd095455499e4d773655edcc0
MecaCho <1099833328@qq.com>
2020-02-05 11:33:25 +0800

    add validate string length;

--- pkg/model/post.go
+++ pkg/model/post.go
@@ -4,12 +4,12 @@ package model
 type Post struct {
 	// gorm.Model
 	BaseCommonModel
-	Title    string `json:"title" gorm:"size:128;not null;unique" binding:"required"`
+	Title    string `json:"title" gorm:"size:128;not null;unique" binding:"required,lt=128"`
 	Content  string `json:"content" gorm:"type:text;not null" binding:"required"`
-	Author   string `json:"author" gorm:"size:32;not null"`
-	Category string `json:"category" gorm:"size:32;not null" binding:"required"`
-	Read     int64  `json:"read"`
-	Comment  int64  `json:"comment"`
+	Author   string `json:"author" gorm:"size:32;not null" binding:"lt=32"`
+	Category string `json:"category" gorm:"size:32;not null" binding:"required,lt=32"`
+	Read     int64  `json:"read" binding:"max=10,min=0"`
+	Comment  int64  `json:"comment" binding:"max=10,min=0"`
 }
 
 func (p *Post) TableName() string {
